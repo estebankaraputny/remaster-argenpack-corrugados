85f5ebd2cdb69f98b660d5fa1e003c034a742f0c
estebankaraputny <estebankaraputny18@gmail.com>
2026-01-15 12:05:00 -0300

    :sparkles: Fix: optimizacion de imagenes

---
+++ src/page/BoxVisualizer/BoxVisualizer.tsx
@@ -0,0 +1,11 @@
+import BoxMeasures from "../../components/BoxMeasures/BoxMeasures";
+
+const BoxVisualizer = () => {
+    return (
+        <div>
+            <BoxMeasures />
+        </div>
+    );
+}
+
+export default BoxVisualizer;
