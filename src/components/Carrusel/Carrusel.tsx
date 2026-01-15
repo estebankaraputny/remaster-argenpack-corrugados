2c540557a702d05eb265b63f8e82f29105a610da
estebankaraputny <estebankaraputny18@gmail.com>
2026-01-15 12:59:15 -0300

    Corregir mayusculas en carrusel

--- src/components/Carrusel/Carrusel.tsx
+++ src/components/Carrusel/Carrusel.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef, useMemo, useCallback } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
-import "./carrusel.css";
+import "./Carrusel.css";
 
 type ImageType = string;
 
